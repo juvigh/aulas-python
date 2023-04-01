fb8718f6ae4154f44198ab9d681ec74dd5fdd1d9
Julio <juliolk.4321@gmail.com>
2023-04-01 17:56:41 -0300

    ex add aula 1

---
+++ aula-1.py
@@ -0,0 +1,72 @@
+
+# --- exercicio 1 ---
+
+
+# Classe: Poligono
+#    Atributos: 
+#     - lado
+#     - numero de lados
+#     - apótema
+    
+#     Métodos:
+#     - Calcular perímetro
+#     - Calcular área
+
+
+# --- exercicio 2 ---
+
+
+# Classe: PatoDeBorracha
+#   Atributos: 
+#     - cor
+#     - tamanho
+#     - peso
+#     - material
+#     - nome
+
+#   Metódos:
+#    - emitirSom
+#    - flutuar
+#    - esguicharAgua
+#    - murchar
+#    - esticar
+
+
+# --- exercicio 3 ---
+
+
+# Classe Passaro:
+#  Atributos: 
+#     - penas
+#     - asas
+#     - bico
+#     - coloração
+
+#   Métodos:
+#     - voar
+#     - cantar
+#     - alimentar
+#     - aninhar
+#     - migrar
+
+
+# --- exercicio 4 ---
+
+
+# Classe Aluno:
+#  Atributos: 
+#     - nome
+#     - sobrenome
+#     - idade
+#     - data de nascimento
+
+
+# --- exercicio 5 ---
+
+
+# Car:
+#  Métodos: 
+#     - transportar
+#     - Ligar som
+#     - Abrir portas
+#     - Limpar para-brisa
